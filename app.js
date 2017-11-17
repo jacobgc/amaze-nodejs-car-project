47b9251367a947d19450e239ab883dab615d3a43
Jacob <jacob.clarke2@gmail.com>
2017-11-17 11:04:02 +0000

    Removed unused pages

--- app.js
+++ app.js
@@ -1,20 +1,18 @@
 // Modules
-var compression = require('compression')
-var express = require('express');
-var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
-var helmet = require('helmet');
+const compression = require('compression');
+const express = require('express');
+const path = require('path');
+const favicon = require('serve-favicon');
+const logger = require('morgan');
+const cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser');
+const helmet = require('helmet');
 
 // Routes
-var index = require('./routes/index');
-var about = require('./routes/about');
-var features = require('./routes/features');
-var pricing = require('./routes/pricing');
+const index = require('./routes/index');
 
-var app = express();
+
+const app = express();
 app.use(compression()); // Adds GZIP Compression
 app.use(helmet()); // Sets HTTP headers to secure express
 
@@ -29,14 +27,17 @@ app.use(bodyParser.urlencoded({extended: false}));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use(function (req,res,next){
+    res.locals.env = req.app.get('env');
+    next();
+});
+
 app.use('/', index);
-app.use('/about', about);
-app.use('/features', features);
-app.use('/pricing', pricing);
+
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
-    var err = new Error('Not Found');
+    const err = new Error('Not Found');
     err.status = 404;
     next(err);
 });
--- routes/about/index.js
+++
@@ -1,9 +0,0 @@
-var express = require('express');
-var router = express.Router();
-
-/* GET home page. */
-router.get('/', function (req, res) {
-    res.render('about', {title: 'First Fleet Management -- About'});
-});
-
-module.exports = router;
